fc7a94215dcf455b221442f07425cd694f6b7df9
marinawex <sheep803@gmail.com>
2022-12-21 18:20:19 +0200

    event emitter

---
+++ eventEmitter.ts
@@ -0,0 +1,49 @@
+
+class EventEmmiter {
+    constructor() {
+        this.events = {}
+    }
+
+    subscribe(eventName, handler){
+        if (this.events[eventName]) {
+            this.events[eventName].push handler)
+        } else {
+            this.events[eventName] = [handler]
+        }
+
+    }
+
+    emit(eventName, ...rest ) {
+        if(this.events[eventName]) {
+            this.events[eventName].forEach(ev => {
+                ev.apply(null, rest);
+            });
+        }
+
+    }
+
+    unsubscribe(eventName, handlerToRemove){
+        return this.events[eventName] = this.events[eventName].filter(handler => handler !== handlerToRemove);
+        
+
+    }
+}
+
+const EventEmitter = new EventEmmiter;
+
+function logKeydown (event) {
+    console.log('keyboard pressed: ', event.key);
+    }
+    function alertKeydown (event) {
+    alert('keyboard pressed: ' + event.key);
+    }
+
+
+
+const logKeydownSubscription = EventEmitter.subscribe('keydown', logKeydown);
+const alertKeydownSubscription = EventEmitter.subscribe('keydown',
+    logKeydown);
+    
+EventEmitter.emit('keydown', {key: 'Enter'});
+// alertKeydownSubscription.unsubscribe();
+// EventEmitter.emit('keydown', {key: 'Left'});
